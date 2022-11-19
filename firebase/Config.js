07d20cd56a29dfd0d7defc179c5a7dc8b8eea0f1
Felipe Jaramillo <Artimax85@gmail.com>
2022-11-18 19:22:27 -0500

    Firebase authentification login and register

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,15 +1,15 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
-import Constants from "expo-constants";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVep0VBEbHu5hk7VXver5vE4d9BimudtA",
+  authDomain: "c8-79-react-native.firebaseapp.com",
+  projectId: "c8-79-react-native",
+  storageBucket: "c8-79-react-native.appspot.com",
+  messagingSenderId: "326484106808",
+  appId: "1:326484106808:web:248533195fa692282510cd"
 };
 
-initializeApp(firebaseConfig);
-export const database = getFirestore()  
