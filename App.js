193ec5d90c40fb11faf35eadadf11a703c360059
Felipe Jaramillo <Artimax85@gmail.com>
2022-11-24 17:50:59 -0500

    Error Arreglado

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PasswordRecovery from "./screens/PasswordRecovery";
 import Register from "./screens/Register";
-import Menu from "./navigation/Menu";
+import Menu from './Navigation/Menu'
 import Login from "./screens/Login";
 import NewClient from "./screens/NewClient";
 
