f4a3e9175b41c83697a0dcabacbac39170b86d3c
lucas <mrlucaszarate@gmail.com>
2022-12-04 20:48:09 -0300

    Modificar estilo de drawer

--- navigation/CustomMenu.js
+++ navigation/CustomMenu.js
@@ -8,67 +8,88 @@ import React from "react";
 import { Icon } from "@rneui/base";
 import { Avatar } from "@rneui/themed";
 import { signOutUser } from "../firebase/session";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CustomMenu = (props) => {
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItem
-      onPress={()=>props.navigation.navigate("Perfil")}
-        label={() => (
-          <View>
-            <View
-              style={{
-                marginTop: -10,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Avatar
-                containerStyle={{ margin: 0, maxWidth: "50%" }}
-                size={100}
-                rounded
-                source={{
-                  uri: "https://randomuser.me/api/portraits/men/36.jpg",
-                }}
-              />
-              
-              <Text
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{ backgroundColor: "#A1D6E2" }}
+      >
+        <DrawerItem
+          onPress={() => props.navigation.navigate("Perfil")}
+          label={() => (
+            <View>
+              <View
                 style={{
-                  fontSize: 22,
-                  flexWrap: "wrap",
-                  width: "50%",
-                  textAlign: "center",
-                  marginTop: 0,
-                  marginLeft: 10,
+                  marginTop: -10,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
               >
-                Bienvenido Nombre
-              </Text>
-            </View>
-          </View>
-        )}
-        style={{
-          width: "95%",
-          height: 110,
-          borderBottomWidth: 1,
-          borderBottomColor: "#000",
-        }}
-      />
+                <Avatar
+                  containerStyle={{ margin: 0, maxWidth: "50%" }}
+                  size={100}
+                  rounded
+                  source={{
+                    uri: "https://randomuser.me/api/portraits/men/36.jpg",
+                  }}
+                />
 
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Logout"
-         onPress={() => signOutUser() }//props.navigation.replace("Login")
-        icon={() => <Icon name="exit" type="ionicon"></Icon>}
+                <Text
+                  style={{
+                    fontSize: 20,
+                    flexWrap: "wrap",
+                    width: "50%",
+                    textAlign: "center",
+                    marginTop: 0,
+                    marginLeft: 10,
+                  }}
+                >
+                  Bienvenido Nombre
+                </Text>
+              </View>
+            </View>
+          )}
+          style={{
+            width: "95%",
+            height: 110,
+            borderBottomWidth: 1,
+            borderBottomColor: "#ccc",
+          }}
+        />
+        <View style={{ flex: 1, backgroundColor: "#fff", paddingTop: 10 }}>
+          <DrawerItemList {...props} />
+        </View>
+      </DrawerContentScrollView>
+      <View
         style={{
-        
-          borderTopWidth: 0,
-          borderTopColor: "#000",
-          paddingTop: 5,
+          paddingLeft: 25,
+          paddingVertical: 10,
+          borderTopWidth: 1,
+          borderTopColor: "#ccc",
         }}
-      />
-    </DrawerContentScrollView>
+      >
+        <TouchableOpacity
+          onPress={() => signOutUser()}
+          style={{ paddingVertical: 15 }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Icon name="exit" type="ionicon" />
+            <Text
+              style={{
+                fontSize: 15,
+                marginLeft: 30,
+              }}
+            >
+              Cerrar Sesion
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
