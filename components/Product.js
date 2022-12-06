8570970f96b9e1b2913885c6fc08fed375f9c98b
Nuthelk Molina <nuthelkcd@gmail.com>
2022-12-06 15:42:56 -0500

    pantalla products listo con la bd y estilos

--- components/Product.js
+++ components/Product.js
@@ -1,89 +1,69 @@
 import {
-    TouchableHighlight,
-    TouchableOpacity,
-    StyleSheet,
-    Text,
-    View,
-  } from "react-native";
-  import { Avatar, Icon } from "@rneui/themed";
-  import { useNavigation } from "@react-navigation/native";
-  import { collection, getDocs } from "firebase/firestore";
-  import { db } from "../firebase/Config";
-  import { useTheme } from "@react-navigation/native";
-  
-  
-  export const getInitials = (client) => {
-  
-    const firstName = client.firstName[0].toUpperCase();
-    const firstLastName = client.lastName[0].toUpperCase();
-    return firstName + firstLastName;
-  };
-  
-  
-  
-  
-  
-  const Product = ({ fontSz, textColor, item, isPress, index }) => {
-    const {colors}=useTheme()
-    const navigation = useNavigation();
-    
-    
-  
-  
-    return (
-      <TouchableOpacity
-        activeOpacity={0.2}
-        //onPressIn={isPress}
-        onPress={() => {
-          isPress, navigation.navigate("ClientDetail", item);
-        }}
-        style={[
-          index % 2 === 0
-            ? { backgroundColor: colors.primary }
-            : { backgroundColor: colors.background },
-          styles.item,
-        ]}
-      >
-        <View style={styles.contact}>
-          <Avatar
-            size={40}
-            rounded
-            title={getInitials(item)}
-            containerStyle={{ backgroundColor: "#676f72" }}
-          />
+  TouchableHighlight,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Avatar, Icon } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase/Config";
+import { useTheme } from "@react-navigation/native";
+
+const Product = ({ fontSz, textColor, item, isPress, index }) => {
+  const { colors } = useTheme();
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.2}
+      //onPressIn={isPress}
+      // onPress={() => {
+      //   isPress, navigation.navigate("ClientDetail", item);
+      // }}
+      style={[
+        index % 2 === 0
+          ? { backgroundColor: colors.primary }
+          : { backgroundColor: colors.background },
+        styles.item,
+      ]}
+    >
+      <View style={styles.contact}>
+        <View style={{flex: 1, flexDirection:"row", justifyContent:'space-evenly'}}>
+          <Text style={[styles.title, { color: textColor, fontSize: fontSz}]}>
+            {item.nombre}
+          </Text>
           <Text style={[styles.title, { color: textColor, fontSize: fontSz }]}>
-            {item.firstName} {item.lastName}
+            {item.cantidad}
+          </Text>
+          <Text style={[styles.title, { color: textColor, fontSize: fontSz }]}>
+            {`$ ${item.precioVenta}`}
           </Text>
         </View>
-        <Icon
-          style={{ paddingRight: 10 }}
-          name="chevron-right"
-          type="feather"
-          color="#BCBABB"
-        />
-      </TouchableOpacity>
-    );
-  };
-  
-  export default Product;
-  
-  const styles = StyleSheet.create({
-    item: {
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-      height: 60,
-      padding: 5,
-      marginVertical: 0,
-      marginHorizontal: 0,
-    },
-    title: {
-      paddingLeft: 20,
-    },
-    contact: {
-      flexDirection: "row",
-      alignItems: "center",
-    },
-  });
-  
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default Product;
+
+const styles = StyleSheet.create({
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 60,
+    marginVertical: 0,
+    marginHorizontal: 0,
+  },
+  title: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  contact: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
